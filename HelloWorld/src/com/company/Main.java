4a5f2b5eafd745ecbc71d2a46f80b76921759bbf
Erin Stoker <erinstokermusic@gmail.com>
2020-01-07 14:05:03 -0500

    finished(?) hello world

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Erin");
+
+        //print three more lines abt what you did during the summer
+        System.out.print("During the summer, I went to OWJL Camp,\n" +
+                "and learned many new skills, such as felting " +
+                "and debate skills.\n");
+        System.out.print("I also went to an OSUMB clinic," +
+                " and learned how the Ohio State\nUniversity " +
+                "Marching Band runs.\n");
+        System.out.println("I also went to band camp with the" +
+                " Licking Heights Marching Band,\n" +
+                "and learned most of the show.");
     }
 }
