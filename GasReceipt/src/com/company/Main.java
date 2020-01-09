ee954227e9845020f65a7c49233b60de493a86f9
Erin Stoker <erinstokermusic@gmail.com>
2020-01-09 14:09:06 -0500

    gas receipt

--- GasReceipt/src/com/company/Main.java
+++ GasReceipt/src/com/company/Main.java
@@ -7,17 +7,18 @@ public class Main {
         System.out.println("|                             |");
         System.out.println("|                             |");
         System.out.println("|                             |");
+        System.out.println("| Date             Time       |");
+        System.out.println("| 9JAN2020         4:00 PM    |");
+        System.out.println("| Invoice Number#    324886   |");
+        System.out.println("| Auth#                       |");
+        System.out.println("| MasterCard                  |");
+        System.out.println("| ***********38               |");
+        System.out.println("| Pump      Product      $/G  |");
+        System.out.println("| 05        unleaded     $2.68|");
+        System.out.println("| Amount     Fuel Total       |");
+        System.out.println("| 12.00       $32.16          |");
         System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
+        System.out.println("|     Thanks For Using        |");
         System.out.println("|                             |");
         System.out.println("+-----------------------------+");
 
