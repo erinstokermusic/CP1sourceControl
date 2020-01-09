23a75c4f5391d6b694f7124e487424dae757ac21
Erin Stoker <erinstokermusic@gmail.com>
2020-01-09 14:15:27 -0500

    gas receipt finished

--- GasReceipt/src/com/company/Main.java
+++ GasReceipt/src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("+-----------------------------+");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
-        System.out.println("|                             |");
+        System.out.println("|          Coral Gas          |");
+        System.out.println("| 7689 Rydal Poplars Dr       |");
+        System.out.println("| Blacklick OH 43004          |");
         System.out.println("| Date             Time       |");
         System.out.println("| 9JAN2020         4:00 PM    |");
         System.out.println("| Invoice Number#    324886   |");
-        System.out.println("| Auth#                       |");
+        System.out.println("| Auth#              354624   |");
         System.out.println("| MasterCard                  |");
         System.out.println("| ***********38               |");
         System.out.println("| Pump      Product      $/G  |");
@@ -18,8 +18,8 @@ public class Main {
         System.out.println("| Amount     Fuel Total       |");
         System.out.println("| 12.00       $32.16          |");
         System.out.println("|                             |");
-        System.out.println("|     Thanks For Using        |");
-        System.out.println("|                             |");
+        System.out.println("|       Thanks For Using      |");
+        System.out.println("|          Coral Gas          |");
         System.out.println("+-----------------------------+");
 
     }
