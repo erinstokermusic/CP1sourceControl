a0dc0ed0a6f5e8752b4c9d6e28f1ef1686368e8a
Erin Stoker <erinstokermusic@gmail.com>
2020-01-13 12:33:47 -0500

    mad lib thing

--- MadLib/src/com/company/Main.java
+++ MadLib/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+// phase one
+        String name;
+        name = "aaaaaa";
+        System.out.println("The variable name is "+ name);
     }
 }
