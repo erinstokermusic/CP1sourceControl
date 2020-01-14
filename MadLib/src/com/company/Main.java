8875721e455f8f0a128fac02bc281528b0af1dd6
Erin Stoker <erinstokermusic@gmail.com>
2020-01-14 12:33:34 -0500

    variables

--- MadLib/src/com/company/Main.java
+++ MadLib/src/com/company/Main.java
@@ -4,8 +4,63 @@ public class Main {
 
     public static void main(String[] args) {
 // phase one
-        String name;
-        name = "aaaaaa";
-        System.out.println("The variable name is "+ name);
+        String usersName;
+        usersName = "a";
+
+        String nounOne;
+        nounOne = "b";
+
+        String nounTwo;
+        nounTwo = "c";
+
+        String nounThree;
+        nounThree = "d";
+
+        String pastTenseVerb;
+        pastTenseVerb = "e";
+
+        String nounFour;
+        nounFour = "f";
+
+        String pastTenseVerbTwo;
+        pastTenseVerbTwo = "g";
+
+        String placeOne;
+        placeOne = "h";
+
+        String placeTwo;
+        placeTwo = "i";
+
+        String adjectiveEDOne;
+        adjectiveEDOne = "j";
+
+        String adjectiveOne;
+        adjectiveOne = "k";
+
+        String pastTenseVerbThree;
+        pastTenseVerbThree = "l";
+
+        String nounFive;
+        nounFive = "m";
+
+        String pastTenseVerbFour;
+        pastTenseVerbFour = "n";
+
+        System.out.println(usersName + "'s " + nounOne + "\n" +
+                "The " + nounTwo + " Eaters\n" +
+                "Now name the lord of " + nounThree +" " + pastTenseVerb + " in the north\n" +
+                "a(n) " + nounFour + " against the ships, and driving veils\n" +
+                "of squall " + pastTenseVerbTwo + " down like night on " + placeOne + " and " + placeTwo + ".\n" +
+                "The bows went plunging at the gust; sails\n" +
+                adjectiveEDOne + " and lashed out strips in the " + adjectiveOne + " wind.\n" +
+                "We " + pastTenseVerbThree + " death in that " + nounFive + ", " + pastTenseVerbFour + " the yards,\n" +
+                "unshipped the oars, and pulled for the nearest lee:\n" +
+                "then two long days and nights we lay adverb\n" +
+                "worn out and adjective at heart, tasting our grief,\n" +
+                "until a number value i.e. first, second, etc. dawn came with ringlets shining.\n" +
+                "Then we put up our masts, past tense verb sail, and rested,\n" +
+                "letting the noun and the noun take over.\n" +
+                "\n");
+
     }
 }
