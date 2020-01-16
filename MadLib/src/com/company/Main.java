986aa3a11a3eb1a8ac07a50cc089a2a6ca7b51b9
Erin Stoker <erinstokermusic@gmail.com>
2020-01-16 12:43:38 -0500

    mad lib done

--- MadLib/src/com/company/Main.java
+++ MadLib/src/com/company/Main.java
@@ -1,50 +1,118 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-// phase one
+
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        // phase one
+
         String usersName;
-        usersName = "a";
+
 
         String nounOne;
-        nounOne = "b";
+
 
         String nounTwo;
-        nounTwo = "c";
+
 
         String nounThree;
-        nounThree = "d";
+
 
         String pastTenseVerb;
-        pastTenseVerb = "e";
+
 
         String nounFour;
-        nounFour = "f";
+
 
         String pastTenseVerbTwo;
-        pastTenseVerbTwo = "g";
+
 
         String placeOne;
-        placeOne = "h";
+
 
         String placeTwo;
-        placeTwo = "i";
+
 
         String adjectiveEDOne;
-        adjectiveEDOne = "j";
+
 
         String adjectiveOne;
-        adjectiveOne = "k";
+
 
         String pastTenseVerbThree;
-        pastTenseVerbThree = "l";
+
 
         String nounFive;
-        nounFive = "m";
+
 
         String pastTenseVerbFour;
-        pastTenseVerbFour = "n";
+
+
+        String adverb;
+
+
+        String adjectiveTwo;
+
+
+        String numberOne;
+
+
+        String pastTenseVerbFive;
+
+
+        String nounSix;
+
+
+        String nounSeven;
+
+
+        //phase two
+        System.out.println("What's your name?");
+        usersName = keyboard.nextLine();
+        System.out.println("Give me a noun.");
+        nounOne = keyboard.nextLine();
+        System.out.println("Give me another noun.");
+        nounTwo = keyboard.nextLine();
+        System.out.println("Another noun.");
+        nounThree = keyboard.nextLine();
+        System.out.println("Another noun.");
+        nounFour = keyboard.nextLine();
+        System.out.println("Another one. We need a lot of nouns.");
+        nounFive = keyboard.nextLine();
+        System.out.println("Another noun.");
+        nounSix = keyboard.nextLine();
+        System.out.println("Last one.");
+        nounSeven = keyboard.nextLine();
+        System.out.println("Give me a verb in the past tense.");
+        pastTenseVerb = keyboard.nextLine();
+        System.out.println("Another past tense verb.");
+        pastTenseVerbTwo = keyboard.nextLine();
+        System.out.println("Another one. We need a lot of these too.");
+        pastTenseVerbThree = keyboard.nextLine();
+        System.out.println("Another one.");
+        pastTenseVerbFour = keyboard.nextLine();
+        System.out.println("Another one.");
+        pastTenseVerbFive = keyboard.nextLine();
+        System.out.println("Give me an adjective.");
+        adjectiveOne = keyboard.nextLine();
+        System.out.println("Give me another adjective.");
+        adjectiveTwo = keyboard.nextLine();
+        System.out.println("Give me an adjective ending in ed.");
+        adjectiveEDOne = keyboard.nextLine();
+        System.out.println("Give me an adverb.");
+        adverb = keyboard.nextLine();
+        System.out.println("Give me the name of a place. It can be as specific or as general as you want.");
+        placeOne = keyboard.nextLine();
+        System.out.println("Give me a second place.");
+        placeTwo = keyboard.nextLine();
+        System.out.println("Give me a number, but in the style of first, second, etc.");
+        numberOne = keyboard.nextLine();
 
         System.out.println(usersName + "'s " + nounOne + "\n" +
                 "The " + nounTwo + " Eaters\n" +
@@ -55,11 +123,11 @@ public class Main {
                 adjectiveEDOne + " and lashed out strips in the " + adjectiveOne + " wind.\n" +
                 "We " + pastTenseVerbThree + " death in that " + nounFive + ", " + pastTenseVerbFour + " the yards,\n" +
                 "unshipped the oars, and pulled for the nearest lee:\n" +
-                "then two long days and nights we lay adverb\n" +
-                "worn out and adjective at heart, tasting our grief,\n" +
-                "until a number value i.e. first, second, etc. dawn came with ringlets shining.\n" +
-                "Then we put up our masts, past tense verb sail, and rested,\n" +
-                "letting the noun and the noun take over.\n" +
+                "then two long days and nights we lay " + adverb + "\n" +
+                "worn out and " + adjectiveTwo + " at heart, tasting our grief,\n" +
+                "until a " + numberOne + " dawn came with ringlets shining.\n" +
+                "Then we put up our masts, " + pastTenseVerbFive + " sail, and rested,\n" +
+                "letting the " + nounSix + " and the " + nounSeven + " take over.\n" +
                 "\n");
 
     }
