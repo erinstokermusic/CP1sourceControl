7e87ee212c9da3579ce84dff64bbfa4a6587ceef
Erin Stoker <erinstokermusic@gmail.com>
2020-01-16 12:10:37 -0500

    error check

---
+++ ExampleCode/ErrorCheck1/ErrorCheck1/src/org/LickingHeights/Main.java
@@ -0,0 +1,23 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("We discussed about the different type of " +
+                "runtime errors");
+
+        System.out.println("There are semi-colon errors");
+
+        System.out.println("Two kinds of bracket errors");
+
+        System.out.println("which are either missing a matching pair,");
+        System.out.println("or are in the wrong place");
+    }
+        system.out.println("Typing commands that are not recognized because of spelling mistakes" +
+                "or capitalization errors are also runtime errors");
+
+    }
+
+
