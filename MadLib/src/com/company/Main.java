34db3913390a4d3e7d4eeb3613cd1934f069d901
Erin Stoker <erinstokermusic@gmail.com>
2020-01-17 12:29:48 -0500

    mad lib done, but for real

--- MadLib/src/com/company/Main.java
+++ MadLib/src/com/company/Main.java
@@ -15,6 +15,9 @@ public class Main {
         String usersName;
 
 
+        String name;
+
+
         String nounOne;
 
 
@@ -71,52 +74,97 @@ public class Main {
 
         String nounSeven;
 
-
         //phase two
+
         System.out.println("What's your name?");
+
         usersName = keyboard.nextLine();
+
+        System.out.println("What's the name of your best friend?");
+
+        name = keyboard.nextLine();
+
         System.out.println("Give me a noun.");
+
         nounOne = keyboard.nextLine();
+
         System.out.println("Give me another noun.");
+
         nounTwo = keyboard.nextLine();
+
         System.out.println("Another noun.");
+
         nounThree = keyboard.nextLine();
+
         System.out.println("Another noun.");
+
         nounFour = keyboard.nextLine();
+
         System.out.println("Another one. We need a lot of nouns.");
+
         nounFive = keyboard.nextLine();
+
         System.out.println("Another noun.");
+
         nounSix = keyboard.nextLine();
+
         System.out.println("Last one.");
+
         nounSeven = keyboard.nextLine();
+
         System.out.println("Give me a verb in the past tense.");
+
         pastTenseVerb = keyboard.nextLine();
+
         System.out.println("Another past tense verb.");
+
         pastTenseVerbTwo = keyboard.nextLine();
+
         System.out.println("Another one. We need a lot of these too.");
+
         pastTenseVerbThree = keyboard.nextLine();
+
         System.out.println("Another one.");
+
         pastTenseVerbFour = keyboard.nextLine();
+
         System.out.println("Another one.");
+
         pastTenseVerbFive = keyboard.nextLine();
+
         System.out.println("Give me an adjective.");
+
         adjectiveOne = keyboard.nextLine();
+
         System.out.println("Give me another adjective.");
+
         adjectiveTwo = keyboard.nextLine();
+
         System.out.println("Give me an adjective ending in ed.");
+
         adjectiveEDOne = keyboard.nextLine();
+
         System.out.println("Give me an adverb.");
+
         adverb = keyboard.nextLine();
+
         System.out.println("Give me the name of a place. It can be as specific or as general as you want.");
+
         placeOne = keyboard.nextLine();
+
         System.out.println("Give me a second place.");
+
         placeTwo = keyboard.nextLine();
+
         System.out.println("Give me a number, but in the style of first, second, etc.");
+
         numberOne = keyboard.nextLine();
+
+        //phase three
 
         System.out.println(usersName + "'s " + nounOne + "\n" +
                 "The " + nounTwo + " Eaters\n" +
-                "Now name the lord of " + nounThree +" " + pastTenseVerb + " in the north\n" +
+                "Now " + name + " the lord of " + nounThree +" " + pastTenseVerb + " in the north\n" +
                 "a(n) " + nounFour + " against the ships, and driving veils\n" +
                 "of squall " + pastTenseVerbTwo + " down like night on " + placeOne + " and " + placeTwo + ".\n" +
                 "The bows went plunging at the gust; sails\n" +
