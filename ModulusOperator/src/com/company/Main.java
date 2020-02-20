c71280d32637f3bfe47c7f3f61430103f575466d
Erin Stoker <erinstokermusic@gmail.com>
2020-02-20 11:02:39 -0500

    OddnessMethodCreated

--- ModulusOperator/src/com/company/Main.java
+++ ModulusOperator/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+evenOrOdd();
+    }
+
+    public static void evenOrOdd() {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int positiveNumber;
+
+        System.out.println("Give me a positive number.");
+        positiveNumber = keyboard.nextInt();
+        System.out.println("Number is odd: "+ (positiveNumber % 2 != 0));
     }
 }
