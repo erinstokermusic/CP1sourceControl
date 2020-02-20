d43bbad9287143962ed702bb343dd6af6a492814
Erin Stoker <erinstokermusic@gmail.com>
2020-02-20 11:19:49 -0500

    keepContainedMethodCreated

--- ModulusOperator/src/com/company/Main.java
+++ ModulusOperator/src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 evenOrOdd();
+keepContained();
     }
 
     public static void evenOrOdd() {
@@ -16,5 +17,15 @@ evenOrOdd();
         System.out.println("Give me a positive number.");
         positiveNumber = keyboard.nextInt();
         System.out.println("Number is odd: "+ (positiveNumber % 2 != 0));
+    }
+
+    public static void keepContained() {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int posNumber;
+
+        System.out.println("Give me a positive number.");
+        posNumber = keyboard.nextInt();
+        System.out.println(posNumber +" contained is: " + ((posNumber%8)+5));
     }
 }
